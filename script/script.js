bdb82032f6f20b223ec5e89f55aae154a0b400c0
klelia <clelia.fradella@gmail.com>
2024-10-30 13:32:28 +0100

    esempi

--- script/script.js
+++ script/script.js
@@ -12,43 +12,42 @@ const ingredienti = [
   "vaniglia",
 ];
 
-const giocataLotto = [23, 44, 50, 90, 13, 7];
 const newArray = []; // array vuoto
 
-// // stampo la lunghezza dell'array ovvero il numero di elemento che l'array contiene
-// console.log(newArray.length); //output 0
-// console.log(ingredienti.length); //output 8
+// stampo la lunghezza dell'array ovvero il numero di elemento che l'array contiene
+console.log(newArray.length); //output 0
+console.log(ingredienti.length); //output 8
 
-// //stampo la cioccolata che è il quarto dell'array di ingredienti
+//stampo la cioccolata che è il quarto dell'array di ingredienti
 
-// console.log(ingredienti[3]);
+console.log(ingredienti[3]);
 
-// //stampo lottavo elemento dell'array di ingredienti
-// console.log(ingredienti[7]);
+//stampo lottavo elemento dell'array di ingredienti
+console.log(ingredienti[7]);
 
-// //modifico l'ottavo elemento dell'array ingredienti
-// ingredienti[7] = "scorzetta di limone";
-// console.log(ingredienti[7]);
+//modifico l'ottavo elemento dell'array ingredienti
+ingredienti[7] = "scorzetta di limone";
+console.log(ingredienti[7]);
 
-// // attenzione oggetti e array vengono copiati per riferimento !!!
-// const ing2 = ingredienti;
-// ing2[7] = "marmellata";
+// attenzione oggetti e array vengono copiati per riferimento !!!
+const ing2 = ingredienti;
+ing2[7] = "marmellata";
 
-// console.dir(ing2);
-// console.log(ingredienti);
+console.dir(ing2);
+console.log(ingredienti);
 
-// //stampo tutti gli elementi di un array
-// for (let i = 0; i < ingredienti.length; i++) {
-//   console.log(ingredienti[i]);
-// }
+//stampo tutti gli elementi di un array
+for (let i = 0; i < ingredienti.length; i++) {
+  console.log(ingredienti[i]);
+}
 
-// //stampo il tipo di dato
+//stampo il tipo di dato
 
-// console.dir(Math);
-// console.log(typeof ingredienti);
-// const pippo = "personaggio fumetti";
-// console.log(!newArray.length); //sapere se è vuoto output true è vero che è vuoto
-// console.log(!ingredienti.length); //output false è falso che è vuoto
+console.dir(Math);
+console.log(typeof ingredienti);
+const pippo = "personaggio fumetti";
+console.log(!newArray.length); //sapere se è vuoto output true è vero che è vuoto
+console.log(!ingredienti.length); //output false è falso che è vuoto
 
 // stringa vuota, 0, undefined sono false
 
@@ -92,7 +91,7 @@ if (index2 !== -1) {
 
 // clone di array
 
-const copiDiIngredienti = ingredienti.slice();
+const copiaDiIngredienti = ingredienti.slice();
 
 let copiaDiArray = [];
 for (let i = 0; i < ingredienti.length; i++) {
@@ -100,11 +99,20 @@ for (let i = 0; i < ingredienti.length; i++) {
   copiaDiArray.push(elemento);
 }
 
-console.log(copiDiIngredienti);
+console.log(copiaDiIngredienti);
 console.log(copiaDiArray);
 
 copiaDiArray.pop();
 
-console.log(copiDiIngredienti);
+console.log(copiaDiIngredienti);
 console.log(copiaDiArray);
 console.log(ingredienti);
+
+const numbers = [23, 44, 50, 90, 13, 7];
+
+const numbersSaquare = [];
+for (let i = 0; i < numbers.length; i++) {
+  const numSquare = numbers[i] * numbers[i];
+
+  numbersSaquare.push(numSquare);
+}
